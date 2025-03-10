83b5af4553310a19d83288bb89e2b82579851081
Vengadesh <vengadesan811@gmail.com>
2025-03-10 16:12:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('.discover-now').addEventListener('click', () => {
+    alert('Discover Now button clicked!');
+});
+
+document.querySelector('.explore-now').addEventListener('click', () => {
+    alert('Explore Now button clicked!');
+});
